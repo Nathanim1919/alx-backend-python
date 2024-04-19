c8912c75c3739c56089397dfbc1e50443d272572
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-19 16:27:19 +0300

    Basic annotation -  floor

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """ type-annotated function add that takes a float a and a float b as arguments
 and returns their sum as a float."""
 
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """a type-annotated function concat that takes
  a string str1 and a string str2 as arguments and returns a concatenated string"""
 
