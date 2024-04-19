9e3db21b1444f8065f7d4d4d11aefbd5d4068b79
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-19 17:12:44 +0300

    Complex types - mixed list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,6 +6,7 @@ and returns their sum as a float."""
 
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """returns the sum of a list of floats"""
     return sum(input_list)
