f0743d5b4d658385b6b5d46f72cd64095a5b588b
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-13 15:51:02 +0300

    Parameterize a unti test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a',), KeyError),
+        ({'a': 1}, ('a', 'b'), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test access nested map exception"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
