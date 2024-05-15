f03772db6d5d9bb258b0e6af32420ba26807fd98
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-15 11:00:29 +0300

    more paching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,8 +5,8 @@
 
 import unittest
 from parameterized import parameterized
-from client import GithubOrgClient
-from unittest.mock import patch, PropertyMock
+from client import (GithubOrgClient)
+from unittest.mock import MagicMock, patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -40,6 +40,59 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient('google')._public_repos_url,
                 'https://api.github.com/orgs/google/repos',
             )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """Test public repos"""
+        test_payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 7697149,
+                    "name": "episodes.dart",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/episodes.dart",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 22,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 8566972,
+                    "name": "kratu",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/kratu",
+                    "created_at": "2013-03-04T22:52:33Z",
+                    "updated_at": "2019-11-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 32,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        mock_get_json.return_value = test_payload["repos"]
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock
+                ) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient('google').public_repos(),
+                ["episodes.dart", "kratu"],
+            )
+            mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once()
 
 
 if __name__ == "__main__":
