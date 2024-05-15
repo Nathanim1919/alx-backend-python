be0f4444cc9ca50c7b6b698ee3f24ad0cea6ba19
Nathanim1919 <nathanim2tadele@gmail.com>
2024-05-15 10:04:30 +0300

    Mocking property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -26,3 +26,21 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             'https://api.github.com/orgs/{}'.format(org_name)
         )
+
+    def test_public_repos_url(self) -> None:
+        """Test public repos url"""
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock,
+        ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': 'https://api.github.com/orgs/google/repos'
+            }
+            self.assertEqual(
+                GithubOrgClient('google')._public_repos_url,
+                'https://api.github.com/orgs/google/repos',
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
